5e2fa6356e3462927e3e53d4e73555a769f66a90
lee <2864172651@qq.com>
2021-01-06 23:31:49 +0800

    update

---
+++ Java语言程序设计答案/chapter8/Exercise_08_10.java
@@ -0,0 +1,60 @@
+package chapter8;
+
+import java.util.Random;
+
+public class Exercise_08_10 {
+    public static void main(String[] args) {
+        int[][] numbers = new int[4][4];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                numbers[i][j] = new Random().nextInt(2);
+                System.out.print(numbers[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println("The largest row index: " + largestRow(numbers));
+        System.out.println("The largest column index: " + largestColumn(numbers));
+    }
+
+    public static int largestRow(int[][] numbers) {
+        int rowMax = 0;
+        int max = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            int count = 0;
+            for (int j = 0; j < numbers[i].length; j++) {
+                if (numbers[i][j] == 1) {
+                    count++;
+                }
+            }
+
+            if (count > max) {
+                max = count;
+                rowMax = i;
+            }
+        }
+
+        return rowMax;
+    }
+
+    public static int largestColumn(int[][] numbers) {
+        int columnMax = 0;
+        int max = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            int count = 0;
+            for (int j = 0; j < numbers[i].length; j++) {
+                if (numbers[j][i] == 1) {
+                    count++;
+                }
+            }
+
+            if (count > max) {
+                max = count;
+                columnMax = i;
+            }
+        }
+
+        return columnMax;
+    }
+}
---
+++ Java语言程序设计答案/chapter8/Exercise_08_16.java
@@ -0,0 +1,73 @@
+package chapter8;
+
+public class Exercise_08_16 {
+    public static void main(String[] args) {
+        int[][] numbers = { { 4, 2 }, { 1, 7 }, { 4, 5 }, { 1, 2 }, { 1, 1 }, { 4, 1 } };
+
+        sort(numbers);
+    }
+
+    public static void sort(int m[][]) {
+        int[] tem;
+
+        lo: while (true) {
+            for (int i = 0; i < m.length - 1; i++) {
+                if (m[i][0] > m[i + 1][0]) {
+                    tem = m[i];
+                    m[i] = m[i + 1];
+                    m[i + 1] = tem;
+                }
+
+                if (m[i][0] == m[i + 1][0]) {
+                    if (m[i][1] > m[i + 1][1]) {
+                        tem = m[i];
+                        m[i] = m[i + 1];
+                        m[i + 1] = tem;
+                    }
+                }
+            }
+
+            boolean isSort = true;
+
+            for (int i = 0; i < m.length - 1; i++) {
+                if (m[i][0] < m[i + 1][0]) {
+                    continue;
+                } else if (m[i][0] == m[i + 1][0]) {
+                    if (m[i][1] <= m[i + 1][1]) {
+                        continue;
+                    } else {
+                        isSort = false;
+                        break;
+                    }
+                } else {
+                    isSort = false;
+                    break;
+                }
+            }
+
+            if (isSort) {
+                break lo;
+            }
+        }
+
+        System.out.print("{");
+        for (int i = 0; i < m.length; i++) {
+            System.out.print("{");
+            for (int j = 0; j < m[i].length; j++) {
+                if (j != m[i].length - 1) {
+                    System.out.print(m[i][j] + ", ");
+                } else {
+                    System.out.print(m[i][j]);
+                }
+            }
+            System.out.print("}");
+
+            if (i != m.length - 1) {
+                System.out.print(", ");
+            } else {
+                System.out.print("");
+            }
+        }
+        System.out.println("}");
+    }
+}
